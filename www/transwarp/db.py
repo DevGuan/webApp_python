f168f80f198dedb843729f9c2d97c0cf9be58666
管复生 <975081801@qq.com>
2016-12-20 00:07:24 +0800

    临时保存

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -458,7 +458,7 @@ def update(sql, *args):
 if __name__ == '__main__':
 
     logging.basicConfig(level=logging.DEBUG)
-    creat_engine('www-data','www-data', 'test')
+    creat_engine('root','Gfs1655030139', 'test')
     update('drop table if exits user')
     update('create table user (id int primary key, name text, email text, password text, last_modified real)')
     # import doctest
